6bcc51fc084cfdc1f5cd2aad23ff526a62820183
Loic Lion <loic.lion.contact@gmail.com>
2023-04-26 09:26:06 +0200

    ajout du no refresh pour le form

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,12 @@ import { checkbox } from "./modules/checkbox-task.js";
 
 //////////////////VARIABLES////////////////////////
 const boutonAd = document.getElementById("bouton-add");
-
+const form = document.querySelector("form");
 ////////////////PROGRAMME///////////////////
 boutonAd.addEventListener("click", nouvelleTache);
 addDeleteListener(".container");
 checkbox(".container");
+/////PAS DE REFRESH DES FORMs/////////////////
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
