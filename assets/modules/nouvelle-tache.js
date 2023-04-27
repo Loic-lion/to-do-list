9763b453bf6c7364aa7aae063fb78092367546d6
Loic Lion <loic.lion.contact@gmail.com>
2023-04-27 14:41:16 +0200

    changement du bouton delete

--- assets/modules/nouvelle-tache.js
+++ assets/modules/nouvelle-tache.js
@@ -9,7 +9,7 @@ export function nouvelleTache() {
     return false;
   }
   const newTask = document.createElement("li");
-  newTask.innerHTML = `<input type="checkbox" class="check"> </input> <p>${inputTask}</p> <button type="submit" class="delete hidden"><img src="assets/img/xwhite.png"></button> `;
+  newTask.innerHTML = `<input type="checkbox" class="check"> </input> <p>${inputTask}</p> <button type="button" class="delete hidden"></button> `;
   newTask.classList.add("animate");
   list.appendChild(newTask);
 
